323730fedb1546fe64be60fbabef6a1da3c9533a
VINCENTLE <vle3@uco.edu>
2021-11-03 18:50:18 -0500

    complete implementation

---
+++ view/MyCanvas.java
@@ -0,0 +1,40 @@
+package view;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+import model.GameElement;
+
+public class MyCanvas extends JPanel
+{
+    private GameBoard gameBoard;
+    private ArrayList<GameElement> figures = new ArrayList<>();
+
+    public MyCanvas(GameBoard gameBoard, int width, int height)
+    {
+        this.gameBoard = gameBoard;
+        setPreferredSize(new Dimension(width, height));
+        setBackground(Color.BLACK);
+    }
+
+    @Override 
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        for(var f: figures)
+        {
+            f.render(g2);
+        }
+    }
+    public ArrayList<GameElement> getFigures() {
+        return figures;
+    }
+}
